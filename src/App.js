20ebaecd72153c70fff50f2473e80323d1cb05e6
k.stopczynska <k.stopczynska@gmail.com>
2023-03-15 08:07:28 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="text-white font-bold bg-black h-screen flex flex-auto flex-col items-center justify-center">
       <h1 className="text-6xl m-20">Hello dareITgirl!</h1>
       <p className="text-3xl">Hope you doing great today!</p>
-      <p className="text-3xl">Let's do some codin':D</p>
+      <p className="text-3xl">Let's do some codin':D:D</p>
     </div>
   );
 }
