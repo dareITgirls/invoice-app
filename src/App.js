7dda529009b47106849a8b3867426659c248e9d1
k.stopczynska <k.stopczynska@gmail.com>
2023-03-15 07:54:14 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="text-white font-bold bg-black h-screen flex flex-auto flex-col items-center justify-center">
+      <h1 className="text-6xl m-20">Hello dareITgirl!</h1>
+      <p className="text-3xl">Hope you doing great today!</p>
+      <p className="text-3xl">Let's do some codin':D</p>
+    </div>
+  );
+}
+
+export default App;
