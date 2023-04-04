f0eab779496a8e190812a163d5ce1fb477086020
Joanna <jlapa.transport@gmail.com>
2023-04-04 13:56:26 +0200

    EmptyInvoiceList component structure done

---
+++ src/components/EmptyInvoiceList.js
@@ -0,0 +1,14 @@
+const EmptyInvoiceList = () => {
+	return (
+		<section>
+			<img src='/assets/illustration-empty.svg' alt='A person holding a megaphone' />
+			<h2>There is nothing here</h2>
+			<p>
+				Create a new invoice by clicking the
+				<span>New Invoice</span>
+				button and get started
+			</p>
+		</section>
+	);
+};
+export default EmptyInvoiceList;
