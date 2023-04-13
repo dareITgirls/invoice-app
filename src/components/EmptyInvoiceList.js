116627f619b77ac2978ad4bde975bfc645a46720
Joanna <jlapa.transport@gmail.com>
2023-04-13 14:26:22 +0200

    fix right path to img

--- src/components/EmptyInvoiceList.js
+++ src/components/EmptyInvoiceList.js
@@ -1,12 +1,13 @@
-const EmptyInvoiceList = () => {
+import illustrationEmpty from '../assets/illustrationEmpty.svg';
 
+const EmptyInvoiceList = () => {
 	return (
 		<section>
-			<img src='/assets/illustration-empty.svg' alt='A person holding a megaphone' />
+			<img src={illustrationEmpty} alt='A person holding a megaphone' />
 			<h2>There is nothing here</h2>
 			<p>
 				Create a new invoice by clicking the
-				<span>New Invoice</span>
+				<span> New Invoice </span>
 				button and get started
 			</p>
 		</section>
