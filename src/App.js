1ecfe4d77ed51371222d20fbfd96eb9494f2fa1e
Joanna <jlapa.transport@gmail.com>
2023-05-09 11:00:41 +0200

    Home insert to App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import './App.css';
-import {useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {fetchInvoices} from "./store/invoicesActions";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchInvoices } from './store/invoicesActions';
+import Home from './pages/Home';
 
 function App() {
+	//mozecie popatrzec czy wyniki sie wyswietlaja
+	const invoicesList = useSelector(state => state.invoices.entities);
+	const dispatch = useDispatch();
+	console.log(invoicesList);
 
-    //mozecie popatrzec czy wyniki sie wyswietlaja
-    const invoicesList = useSelector(state => state.invoices.entities)
-    const dispatch = useDispatch()
-    console.log(invoicesList)
+	useEffect(() => {
+		dispatch(fetchInvoices());
+	}, []);
 
-    useEffect(() => {
-        dispatch(fetchInvoices())
-    },[]);
-
-
-  return (
-    <div className="text-white font-bold bg-black h-screen flex flex-auto flex-col items-center justify-center">
-      <h1 className="text-6xl m-20">Hello dareITgirl!</h1>
-      <p className="text-3xl">Hope you doing great today!</p>
-      <p className="text-3xl">Let's do some codin':D:D</p>
-    </div>
-  );
+	return <Home />;
 }
 export default App;
