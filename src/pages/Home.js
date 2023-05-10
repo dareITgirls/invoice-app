af6d84181c5309e40e81dd4da90b5ebe4fc10ee6
Joanna <jlapa.transport@gmail.com>
2023-05-10 11:44:10 +0200

    title for sccessibility issues add, desktop styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
+//Future todo - create Wrapper component for DRY rule
+
 import Nav from '../components/Nav';
 
 const Home = () => {
     return (
-        <>
+        <div className='flex flex-col lg:flex-row'>
         <Nav />
-        </>
+        <h1>Welcome</h1>
+        </div>
     )
 }
 
