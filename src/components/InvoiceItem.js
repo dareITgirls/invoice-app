71c2b8210af98f1816b867cc83d35697a301beea
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2023-05-15 13:02:16 +0200

    add <tbody> to InvoiceListItem

--- src/components/InvoiceItem.js
+++ src/components/InvoiceItem.js
@@ -16,26 +16,28 @@ const InvoiceItem = ({ id }) => {
 	return (
 		<li className='bg-light-100 dark:bg-dark-200 shadow-3xl min-h-33 rounded-lg'>
 			<table className='w-full'>
-				<tr className='grid grid-cols-2 grid-rows-item-sm p-6 gap-1'>
-					<td className='text-dark-300 dark:text-light-100 text-md/1 row-start-1 col-start-1 pb-4'>
-						<span className='text-neutral-400'>#</span>
-						{invoice.id}
-					</td>
+				<tbody>
+					<tr className='grid grid-cols-2 grid-rows-item-sm p-6 gap-1'>
+						<td className='text-dark-300 dark:text-light-100 text-md/1 row-start-1 col-start-1 pb-4'>
+							<span className='text-neutral-400'>#</span>
+							{invoice.id}
+						</td>
 
-					<td className='text-neutral-300 dark:text-neutral-200 text-base/1 row-start-2 self-end'>
-						Due {changeDateFormat(invoice.paymentDue)}
-					</td>
+						<td className='text-neutral-300 dark:text-neutral-200 text-base/1 row-start-2 self-end'>
+							Due {changeDateFormat(invoice.paymentDue)}
+						</td>
 
-					<td className='text-neutral-400 dark:text-light-100 text-base/1 row-start-1 col-start-2 justify-self-end self-start leading-3'>
-						{invoice.clientName}
-					</td>
+						<td className='text-neutral-400 dark:text-light-100 text-base/1 row-start-1 col-start-2 justify-self-end self-start leading-3'>
+							{invoice.clientName}
+						</td>
 
-					<td className='text-dark-300 dark:text-light-100 text-md/2 row-start-3 self-end'>£ {invoice.total}</td>
+						<td className='text-dark-300 dark:text-light-100 text-md/2 row-start-3 self-end'>£ {invoice.total}</td>
 
-					<td className='row-start-2 row-end-4 col-start-2 justify-self-end mt-3'>
-						<Label status={invoice.status} />
-					</td>
-				</tr>
+						<td className='row-start-2 row-end-4 col-start-2 justify-self-end mt-3'>
+							<Label status={invoice.status} />
+						</td>
+					</tr>
+				</tbody>
 			</table>
 		</li>
 	);
--- src/store/modalStatusSlice.js
+++ src/store/modalStatusSlice.js
@@ -1,28 +1,22 @@
 import {createSlice} from "@reduxjs/toolkit";
-import {addNewInvoice, deleteInvoice, editInvoice, fetchInvoices} from "./invoicesActions";
 
 const initialState = {
     status: false,
 }
 
-const modalStatusSlice = createSlice({
+const modalStatusSlice = createSlice( {
     name: 'modalStatus',
     initialState,
     reducers: {
         openModal(state, action) {
             state.status = true
         },
-        closeModal(state, action) {
+        closeModal(state,action) {
             state.status = false
-        },
+        }
     }
 });
 
-export const { openModal, closeModal } = modalStatusSlice.actions;
+export const {openModal, closeModal} = modalStatusSlice.actions;
 
-export default modalStatusSlice.reducer;
-
-
-
-
-
+export default modalStatusSlice.reducer
