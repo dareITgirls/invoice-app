7b7d9094dfdcbc168dac8c764399d13d57552ebd
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2023-05-31 21:50:19 +0200

    bug fixed

--- src/UI/Label.js
+++ src/UI/Label.js
@@ -16,7 +16,7 @@ const Label = ({ status }) => {
 	}
 
 	return (
-		<div className='md:w-28 md:mr-6'>
+		<div className='w-28 md:mr-6'>
 			<p className={className}>{firstLetterToUpper(status)}</p>
 		</div>
 		)
