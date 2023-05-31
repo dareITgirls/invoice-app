6bb1acdf02f002c866bd27c29623108e172f4876
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2023-05-31 23:15:16 +0200

    typography ready; working on "new" button

--- src/components/HeaderHome.js
+++ src/components/HeaderHome.js
@@ -28,16 +28,19 @@ const HeaderHome = () => {
 	}
 
 	return (
-		<div className='grid grid-cols-3 grid-rows-2 px-6 md:px-12 pt-8 pb-0 md:pb-0 '>
-			<h1 className=''>Invoices</h1>
-			<div className='flex row-span-2'>
-				<button className='' onClick={openStatusFilterModal}>Filter <span className='hidden md:visible'>by status</span></button>
+		<div className='grid grid-cols-3 grid-rows-2 gap-2 px-6 md:px-12 pt-8 pb-0 md:pb-0 '>
+			<h1 className='text-lg'>Invoices</h1>
+			<div className='flex justify-end items-center row-span-2'>
+				<button className='text-dark-300 dark:text-light-100 text-md/2 mr-3' onClick={openStatusFilterModal}>Filter <span className='hidden md:visible'>by status</span></button>
 				<svg width="11" height="7" xmlns="http://www.w3.org/2000/svg"><path d="M1 1l4.228 4.228L9.456 1" stroke="#7C5DFA" stroke-width="2" fill="none" fill-rule="evenodd"/></svg>
 			</div>
-			<FilterModal/>
-			<button className='row-span-2' onClick={openNewInvoice}>New invoice</button>
-			<NewInvoice />
-			<p className=' md:hidden'>{invoicesNumber} invoices</p>
+			<div role='button' className='flex row-span-2 justify-center items-center row-span-2 bg-primary-200 rounded-3xl'>
+				<div className='bg-light-100 rounded-full p-2'>
+					<svg className='' width="11" height="11" xmlns="http://www.w3.org/2000/svg"><path d="M6.313 10.023v-3.71h3.71v-2.58h-3.71V.023h-2.58v3.71H.023v2.58h3.71v3.71z" fill="#7C5DFA" fill-rule="nonzero"/></svg>
+				</div>
+				<p className='text-light-100 text-md/2' onClick={openNewInvoice}>New <span className='hidden md:visible'>invoice</span></p>
+			</div>
+			<p className='text-neutral-300 text-base/1 md:hidden'>{invoicesNumber} invoices</p>
 			{invoicesNumberInfo}
 		</div>
 	);
