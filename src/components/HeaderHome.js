81e1253518fe2ccff8d9bf50e0c3fe6fe8c72bf3
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2023-06-01 23:52:36 +0200

    <div> changed to <button> for 'filter by status'

--- src/components/HeaderHome.js
+++ src/components/HeaderHome.js
@@ -3,8 +3,6 @@ import { openNewFormModal } from '../store/newFormModalStatusSlice';
 import { openFilterModal } from "../store/filterModalSlice";
 import { ReactComponent as IconPlus } from '../assets/icon-plus.svg'
 import { ReactComponent as IconArrowDown } from '../assets/icon-arrow-down.svg'
-import FilterModal from "./FilterModal";
-import NewInvoice from "./NewInvoice";
 
 const HeaderHome = () => {
 	const dispatch = useDispatch();
@@ -32,14 +30,14 @@ const HeaderHome = () => {
 	return (
 		<div className='grid grid-cols-3 md:grid-cols-4 grid-rows-2'>
 			<h1 className='text-lg dark:text-light-100 md:text-xl md:col-span-2'>Invoices</h1>
-			<div role='button'
+			<button
 				 className='flex justify-end items-center row-span-2'
 				 onClick={openStatusFilterModal}>
 				<div className='text-dark-300 dark:text-light-100 text-md/2 mr-3'>
 					Filter <span className='hidden md:inline'>by status</span>
 				</div>
 				<IconArrowDown/>
-			</div>
+			</button>
 			<div className='flex row-span-2 justify-end items-center'>
 				<button
 					className='flex row-span-2 w-22 h-11 md:w-46 md:h-12 ml-5 justify-around items-center md:self-center row-span-2 bg-primary-200 rounded-3xl'
