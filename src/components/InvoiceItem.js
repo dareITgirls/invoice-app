4094cc6087779fe660bb85eb831ab5c423f53563
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2023-06-05 16:21:59 +0200

    fix conflicts

--- src/components/InvoiceItem.js
+++ src/components/InvoiceItem.js
@@ -1,7 +1,9 @@
 import { useSelector } from "react-redux";
 import { selectInvoiceById } from "../store/invoicesSlice";
 import { ReactComponent as ArrowRight } from "../assets/icon-arrow-right.svg";
+
 import Label from "../UI/Label";
+import { Link } from "react-router-dom";
 
 const InvoiceItem = ({ id }) => {
   const invoice = useSelector((state) => selectInvoiceById(state, id));
@@ -13,31 +15,33 @@ const InvoiceItem = ({ id }) => {
   };
 
   return (
-    <li className='bg-light-100 dark:bg-dark-200 shadow-3xl rounded-lg'>
-      <table className='w-full'>
-        <tbody>
-          <tr className='grid grid-cols-2 md:grid-cols-5 p-6 gap-1'>
-            <td className='text-dark-300 dark:text-light-100 text-md/1 row-start-1 col-start-1 pb-4 md:pb-0 md:self-center'>
-              <span className='text-neutral-400'>#</span>
-              {invoice.id}
-            </td>
-            <td className='text-neutral-300 dark:text-neutral-200 text-base/1 row-start-2 md:row-auto self-end md:self-center'>
-              Due {changeDateFormat(invoice.paymentDue)}
-            </td>
-            <td className='text-neutral-400 dark:text-light-100 text-base/1 row-start-1 md:row-auto justify-self-end md:justify-self-auto self-start md:self-center leading-3'>
-              {invoice.clientName}
-            </td>
-            <td className='text-dark-300 dark:text-light-100 text-md/2 row-start-3 md:row-auto self-end md:pr-10 md:justify-self-end md:self-center'>
-              £ {invoice.total}
-            </td>
-            <td className='row-start-2 row-end-4 col-start-2 md:col-start-5 md:row-auto md:flex md:items-center justify-self-end md:justify-self-center mt-3 md:mt-0'>
-              <Label status={invoice.status} />
-              <ArrowRight className='hidden md:inline' />
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </li>
+    <Link to={`/invoices/${invoice.id}`}>
+      <li className='bg-light-100 dark:bg-dark-200 shadow-3xl rounded-lg'>
+        <table className='w-full'>
+          <tbody>
+            <tr className='grid grid-cols-2 md:grid-cols-5 p-6 gap-1'>
+              <td className='text-dark-300 dark:text-light-100 text-md/1 row-start-1 col-start-1 pb-4 md:pb-0 md:self-center'>
+                <span className='text-neutral-400'>#</span>
+                {invoice.id}
+              </td>
+              <td className='text-neutral-300 dark:text-neutral-200 text-base/1 row-start-2 md:row-auto self-end md:self-center'>
+                Due {changeDateFormat(invoice.paymentDue)}
+              </td>
+              <td className='text-neutral-400 dark:text-light-100 text-base/1 row-start-1 md:row-auto justify-self-end md:justify-self-auto self-start md:self-center leading-3'>
+                {invoice.clientName}
+              </td>
+              <td className='text-dark-300 dark:text-light-100 text-md/2 row-start-3 md:row-auto self-end md:pr-10 md:justify-self-end md:self-center'>
+                £ {invoice.total}
+              </td>
+              <td className='row-start-2 row-end-4 col-start-2 md:col-start-5 md:row-auto md:flex md:items-center justify-self-end md:justify-self-center mt-3 md:mt-0'>
+                <Label status={invoice.status} />
+                <ArrowRight className='hidden md:inline' />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </li>
+    </Link>
   );
 };
 
