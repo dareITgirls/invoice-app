854dd7a770a853858eeae83f545d0323d9b23964
Joanna <jlapa.transport@gmail.com>
2023-07-05 22:59:08 +0200

    error page styles

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,8 +2,10 @@ import { ReactComponent as NotFoundImg } from '../assets/not-found.svg';
 
 export const Error = () => {
 	return (
-		<main className='flex flex-col items-center justify-center font-bold text-center h-screen'>
-			<h1 className='text-lg dark:text-light-100 mt-8 md:text-xl md:mt-20'>Oups. Page not found!</h1>
+		<main className='flex flex-col items-center justify-between gap-6 font-bold text-center h-screen'>
+			<h1 className='text-lg dark:text-light-100 mt-8 md:text-4xl md:mt-20'>Oups!</h1>
+
+			<p className='text-md md:text-lg dark:text-light-100'>Dynamic error info here</p>
 
 			<NotFoundImg />
 		</main>
