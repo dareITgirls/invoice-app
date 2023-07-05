5b11bb26a75a6b9624a32eda6b93e2111cca6868
Joanna <jlapa.transport@gmail.com>
2023-07-05 22:59:28 +0200

    error route added

--- src/index.js
+++ src/index.js
@@ -3,15 +3,22 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 
 import { App } from './App';
+import { Error } from './pages/Error';
 import { store } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<Router>
+				<Routes>
+					<Route path='*' element={<Error />} />
+					<Route path='/' element={<App />} />
+				</Routes>
+			</Router>
+		</Provider>
+	</React.StrictMode>
 );
