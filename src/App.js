d345226d0a793e11d2e86faff3faec515e98d605
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2023-08-03 14:02:56 +0200

    fixed bug with EmptyInvoiceList visible for a short time before loader component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import { useEffect } from 'react';
-import { useDispatch } from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 
 import { Home } from './pages/Home';
 import { fetchInvoices } from './store/invoicesActions';
 
 
 export const App = () => {
-
   const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(fetchInvoices())
     },[dispatch]);
 
-  return (
-    <Home />
-  );
+  return <Home />
 }
 
--- src/components/InvoiceList.js
+++ src/components/InvoiceList.js
@@ -14,7 +14,7 @@ export const InvoiceList = () => {
 		return <InvoiceItem key={id} id={id} />;
 	});
 
-	if ((loadingStatus !== "loading") && (invoicesNumber === 0)) return <EmptyInvoiceList />;
+	if ((loadingStatus !== "none") && (invoicesNumber === 0)) return <EmptyInvoiceList />;
 
 	return <ul className='flex flex-col pt-8 md:pt-14 pb-26 gap-4'>{renderedListItems}</ul>;
 };
