41d4c843b39dd9601e8ac47bb81c7dc4601c7687
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2023-08-03 18:14:42 +0200

    changed styles for HeaderInvoiceView (moved all of the parts that in my opinion belongs to the header to this component: go back button, information about status), fixed padding on buttons, removed the doubbled code (div with edit, delete and mark as unpaid buttons)

--- src/components/HeaderInvoiceView.js
+++ src/components/HeaderInvoiceView.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate, useParams } from 'react-router-dom';
+import {Link, useNavigate, useParams} from 'react-router-dom';
 
 import { AlertModal } from '../components/AlertModal';
 import { deleteInvoice, editInvoice } from '../store/invoicesActions';
@@ -8,6 +8,7 @@ import { toggleInvoiceStatus } from '../store/invoicesSlice';
 import { closeNewFormModal, openNewFormModal } from '../store/newFormModalStatusSlice';
 import { Button } from '../UI/Button';
 import Label from '../UI/Label';
+import BackArrow from "../assets/icon-arrow-left.svg";
 
 const HeaderInvoiceView = () => {
 	const { invoiceId } = useParams();
@@ -45,44 +46,40 @@ const HeaderInvoiceView = () => {
 
 	return (
 		<>
-			<div className='hidden md:flex justify-between bg-light-100 dark:bg-dark-200 w-full items-center px-5 py-5 rounded-lg'>
-				<div className='md:flex items-center'>
+			<Link to={`/`}>
+				<div className='flex items-center my-6 mx-7 sm:mx-auto md:my-10 md:w-4/5 md:m-0 xl:w-1/2 2xl:w-1/3'>
+					<img src={BackArrow} alt='back arrow' className='ml-2'></img>
+					<span className='text-dark-300 dark:text-light-100 text-md/1 ml-3'>Go back</span>
+				</div>
+			</Link>
+			<div className='flex items-center justify-between mx-7 bg-light-100 dark:bg-dark-200 shadow-3xl rounded-lg p-5 sm:hidden'>
+				<span className='text-neutral-500 text-base/2'>Status</span>
+				<Label status={invoice.status} />
+			</div>
+			<div className="fixed sm:static flex items-center justify-between fixed bottom-0 bg-light-100 dark:bg-dark-200 w-full p-5 sm:px-8 rounded-lg">
+				<div className='hidden sm:flex items-center'>
 					<span className='text-neutral-500 text-base/2 md:mr-5'>Status </span>
 					<Label status={invoice.status} />
 				</div>
-				<div className='md:flex items-center gap-2'>
+				<div className="flex w-full sm:w-auto justify-between items-center">
 					<Button
-						styles='bg-neutral-100 text-neutral-500 dark:bg-dark-100 dark:text-white'
+						styles='bg-neutral-100 text-neutral-500 dark:bg-dark-100 dark:text-white hover:bg-neutral-200 px-6 mr-2'
 						title='Edit'
 						onClick={handleEditInvoice}/>
-					<Button styles='bg-red-500 text-white' title='Delete' onClick={showModal} />
+					<Button styles='bg-red-500 text-white hover:bg-danger-50 px-6 mr-2' title='Delete' onClick={showModal} />
 					<Button
-						styles='bg-primary-200 text-white'
+						styles='bg-primary-200 text-white hover:bg-danger-100 px-7'
 						id='markAsPaidButton'
 						title={invoice.status === 'paid' ? 'Mark as Unpaid' : 'Mark as Paid'}
 						onClick={handleChangeStatusInvoice}/>
 				</div>
-			</div>
-			<div className='flex items-center justify-between fixed bottom-0 bg-light-100 dark:bg-dark-200 w-full p-5 md:hidden'>
-				<Button
-					styles='bg-neutral-100 text-neutral-500 dark:bg-dark-100 dark:text-white hover:bg-neutral-200 px-6'
-					title='Edit'
-					onClick={handleEditInvoice}/>
-				<Button styles='bg-red-500 text-white hover:bg-danger-50 px-6' title='Delete' onClick={showModal} />
-				<Button
-					styles='bg-primary-200 text-white hover:bg-danger-100 px-7'
-					id='markAsPaidButton'
-					title={invoice.status === 'paid' ? 'Mark as Unpaid' : 'Mark as Paid'}
-					onClick={handleChangeStatusInvoice}/>
 			</div>
 			<AlertModal invoiceId={invoiceId}>
 				<Button
 					onClick={discardHandler}
 					styles='bg-neutral-100 dark:bg-dark-100 text-neutral-500 dark:text-neutral-200 hover:bg-neutral-200 dark:hover:bg-light-100 dark:hover:text-neutral-500'
 					title='Cancel'
-					type='button'
-				/>
-
+					type='button'/>
 				<Button onClick={handleDeleteInvoice} styles='bg-danger-150 text-light-100 hover:bg-danger-50 dark:hover:bg-danger-50' title='Delete' type='button' />
 			</AlertModal>
 		</>
--- src/pages/InvoiceView.js
+++ src/pages/InvoiceView.js
@@ -22,18 +22,8 @@ const InvoiceView = () => {
 	return (
 		<div inert={modalStatus ? '' : undefined}>
 			<Nav />
-				<Link to={`/`}>
-					<div className='flex items-center my-6 mx-7 md:mx-auto md:my-10 md:w-4/5 md:m-0 xl:w-1/2 2xl:w-1/3'>
-						<img src={BackArrow} alt='back arrow' className='ml-2'></img>
-						<span className='text-dark-300 dark:text-light-100 text-md/1 ml-3'>Go back</span>
-					</div>
-				</Link>
 				<div className='text-dark-300 dark:text-light-100 flex items-center justify-center my-0'>
-					<div className='flex flex-col w-full md:w-4/5 md:m-0 xl:w-1/2 2xl:w-1/3'>
-						<div className='flex items-center justify-between mx-7 bg-light-100 dark:bg-dark-200 shadow-3xl rounded-lg p-5 md:hidden'>
-							<span className='text-neutral-500 text-base/2'>Status </span>
-							<Label status={invoice.status} />
-						</div>
+					<div className='flex flex-col w-full sm:w-4/5 md:m-0 xl:w-1/2'>
 						<HeaderInvoiceView />
 						<InvoiceDetails />
 					</div>
