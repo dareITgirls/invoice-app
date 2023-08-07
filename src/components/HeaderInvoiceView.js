f3a8960dca8fa52648f94ad6637f761f11748ff1
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2023-08-07 12:34:44 +0200

    fixed design up to lg breakpoint

--- src/components/HeaderInvoiceView.js
+++ src/components/HeaderInvoiceView.js
@@ -47,7 +47,7 @@ const HeaderInvoiceView = () => {
 	return (
 		<>
 			<Link to={`/`}>
-				<div className='flex items-center mb-8 sm:mx-auto md:my-10 md:w-4/5 md:m-0 xl:w-1/2 2xl:w-1/3'>
+				<div className='flex items-center mb-8 md:mb-8 md:w-4/5 md:m-0 xl:w-1/2 2xl:w-1/3'>
 					<img src={BackArrow} alt='back arrow' className='ml-2'></img>
 					<span className='text-dark-300 dark:text-light-100 text-md/1 ml-3'>Go back</span>
 				</div>
