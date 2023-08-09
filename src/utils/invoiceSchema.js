27a4a40e4daa2b4bbc030893aa5b41ecf4fc2d3b
k.stopczynska <k.stopczynska@gmail.com>
2023-08-09 10:59:58 +0200

    changed quantity of items rendered to one

--- src/utils/invoiceSchema.js
+++ src/utils/invoiceSchema.js
@@ -39,12 +39,6 @@ export const invoiceSchema = {
             quantity: 0,
             price: 0,
             total: 0
-        },
-          {
-            name: "",
-            quantity: 0,
-            price: 0,
-            total: 0
         }
     ],
     total: 0
