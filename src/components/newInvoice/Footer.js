6100e77fa30601937855fd6e85d218138a528498
k.stopczynska <k.stopczynska@gmail.com>
2023-08-09 12:20:25 +0200

    fixed footer width on mobile

--- src/components/newInvoice/Footer.js
+++ src/components/newInvoice/Footer.js
@@ -19,7 +19,7 @@ export const Footer = () => {
         values.status = 'draft';
         values.paymentDue = getPaymentDue(values.createdAt, values.paymentTerms);
         if (values.items.length > 0) {
-            values.items.map((item) => item.total = getItemTotal(item))
+            values.items.map((item) => item.total === getItemTotal(item))
             values.total = getTotal(values.items);
         }   
         dispatch(addNewInvoice(values));
@@ -28,7 +28,7 @@ export const Footer = () => {
     }
 
     return (
-        <footer className="flex items-center justify-between rounded-xl bg-white dark:bg-dark-300 fixed md:w-[72.8%] md:pr-[56px] bottom-0 pb-4 lg:static lg:w-full lg:pr-0 lg:mt-0 lg:mb-3">
+        <footer className="flex items-center justify-between rounded-xl w-[88%] bg-white dark:bg-dark-300 fixed md:w-[72.8%] md:pr-[56px] bottom-0 pb-4 lg:static lg:w-full lg:pr-0 lg:mt-0 lg:mb-3">
             <Button styles="bg-neutral-100 text-neutral-500" type="button" title="Discard" onClick={discardHandler} />
             <Button styles="bg-dark-500  text-neutral-300 dark:bg-dark-200 dark:text-neutral-200 md:hidden" type="button" title="Save as Draft" onClick={saveAsDraftHandler} />
             <Button styles="bg-primary-200 text-white md:hidden" type="submit" title="Save & Send" onClick={() => setFieldValue('status', 'pending')} />
