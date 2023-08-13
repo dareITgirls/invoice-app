000aff76fe7e6e30aadbf14dda5f0bf310d61ec7
Joanna <jlapa.transport@gmail.com>
2023-08-13 19:09:33 +0200

    GoBack and Nav component inserted into Error

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,13 +1,18 @@
 import { ReactComponent as NotFoundImg } from '../assets/not-found.svg';
-
+import { Nav } from '../components/Nav';
+import { GoBack } from '../UI/GoBack';
 export const Error = () => {
 	return (
-		<main className='flex flex-col items-center justify-between gap-6 font-bold text-center h-screen'>
-			<h1 className='text-lg dark:text-light-100 mt-8 md:text-4xl md:mt-20'>Oups!</h1>
+		<>
+			<Nav />
+			<main className='flex flex-col items-center justify-between gap-6 font-bold text-center h-screen'>
+				<GoBack />
+				<h1 className='text-lg dark:text-light-100 mt-8 md:text-4xl md:mt-20'>Oups!</h1>
 
-			<p className='text-md md:text-lg dark:text-light-100'>Dynamic error info here</p>
+				<p className='text-md md:text-lg dark:text-light-100'>Dynamic error info here</p>
 
-			<NotFoundImg />
-		</main>
+				<NotFoundImg />
+			</main>
+		</>
 	);
 };
