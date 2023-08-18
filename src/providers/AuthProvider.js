f9e9a8aa613b80876374bc861d85663d8f5e2f1b
Joanna <jlapa.transport@gmail.com>
2023-08-18 13:11:36 +0200

    provider AuthProvider created

---
+++ src/providers/AuthProvider.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import { onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth';
+import React, {useContext,useState} from 'react';
+
+import { auth, provider } from '../firebase-config/firebase';
+
+const AuthContext = React.createContext({});
+
+export const AuthProvider = ({children}) => {
+    const [user, setUserData] = useState(null);
+    
+        onAuthStateChanged(auth, user => {
+            if (user) {
+                setUserData(user);
+            } else {
+                setUserData(null);
+            }
+        });
+    
+        const handleSignIn = async () => {
+            try {
+                const authResult = await signInWithPopup(auth, provider);
+            } catch (e) {
+                console.log(e);
+            }
+        };
+    
+        const handleSignOut = async () => {
+            try {
+                await signOut(auth);
+            } catch (e) {
+                console.log(e);
+            }
+        };
+
+    return (<AuthContext.Provider value={{user, handleSignIn, handleSignOut}}>{children}</AuthContext.Provider>
+    )
+}
+
+export const useAuth = () => {
+	const auth = useContext(AuthContext);
+
+	if (!auth) {
+		throw Error('useAuth needs to be used inside AuthContext  Provider');
+	}
+	return auth;
+};
