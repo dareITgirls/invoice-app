09dfbada8ff3cb311dfad5620573432e14a8e384
Joanna <jlapa.transport@gmail.com>
2023-08-18 13:07:45 +0200

    new structure for routes

--- src/index.js
+++ src/index.js
@@ -3,24 +3,36 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 
 import { App } from './App';
+import { ProtectedRoute } from './helpers/ProtectedRoute';
 import { Error } from './pages/Error';
 import InvoiceView from './pages/InvoiceView';
+import { Root } from './pages/Root';
+import { AuthProvider } from './providers/AuthProvider';
 import { store } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path='/' element={<Root />}>
+			<Route path='invoices' element={<ProtectedRoute />}>
+				<Route index element={<App />} />
+			</Route>
+			<Route path='invoices/:invoiceId' element={<ProtectedRoute />}>
+				<Route index element={<InvoiceView />} />
+			</Route>
+			<Route path='*' element={<Error title='The page you’re looking for can’t be found.' />} />
+		</Route>
+	)
+);
 root.render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<Router>
-				<Routes>
-					<Route path='/invoices/:invoiceId' element={<InvoiceView />} />
-					<Route path='/' element={<App />} />
-					<Route path='*' element={<Error title='The page you’re looking for can’t be found.' />} />
-				</Routes>
-			</Router>
-		</Provider>
+		<AuthProvider>
+			<Provider store={store}>
+				<RouterProvider router={router} />
+			</Provider>
+		</AuthProvider>
 	</React.StrictMode>
 );
