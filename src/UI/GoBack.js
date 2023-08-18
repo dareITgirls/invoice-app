f263cafb995f1780471a4d777112fc923bb9e0ea
Joanna <jlapa.transport@gmail.com>
2023-08-18 13:13:12 +0200

    Go back replaced with Invoices for better UX

--- src/UI/GoBack.js
+++ src/UI/GoBack.js
@@ -4,11 +4,11 @@ import BackArrow from '../assets/icon-arrow-left.svg';
 
 export const GoBack = () => {
 	return (
-		<Link to={`/`}>
+		<Link to={`/invoices`}>
 			<div className='flex items-center mb-8 md:mb-8 md:w-4/5 md:m-0 xl:w-1/2 2xl:w-1/3'>
 				<img src={BackArrow} alt='back arrow' className='ml-2'></img>
 
-				<span className='text-dark-300 dark:text-light-100 text-md/1 ml-3'>Go back</span>
+				<span className='text-dark-300 dark:text-light-100 text-md/1 ml-3'>Invoices</span>
 			</div>
 		</Link>
 	);
