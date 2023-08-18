8cec233a97f7c4ea5b5df11a47948bfc00d7e051
Joanna <jlapa.transport@gmail.com>
2023-08-18 14:27:59 +0200

    props title added to GoBack component for better reusability

--- src/UI/GoBack.js
+++ src/UI/GoBack.js
@@ -1,14 +1,15 @@
+/* eslint-disable react/prop-types */
 import { Link } from 'react-router-dom';
 
 import BackArrow from '../assets/icon-arrow-left.svg';
 
-export const GoBack = () => {
+export const GoBack = (props) => {
 	return (
 		<Link to={`/invoices`}>
 			<div className='flex items-center mb-8 md:mb-8 md:w-4/5 md:m-0 xl:w-1/2 2xl:w-1/3'>
 				<img src={BackArrow} alt='back arrow' className='ml-2'></img>
 
-				<span className='text-dark-300 dark:text-light-100 text-md/1 ml-3'>Invoices</span>
+				<p className='text-dark-300 dark:text-light-100 text-md/1 ml-3'>{props.title}</p>
 			</div>
 		</Link>
 	);
