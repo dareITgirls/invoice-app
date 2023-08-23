ed0fba7c0933873dbef4166d31b94b8cc1c1790b
k.stopczynska <k.stopczynska@gmail.com>
2023-08-23 16:03:55 +0200

    added address variant to Input component, change variants in selected inputs

--- src/UI/Input.js
+++ src/UI/Input.js
@@ -8,13 +8,13 @@ export const Input = (props) => {
 
     let classesError = variant === 'full' ? "text-danger-150 text-base/2 mt-[-4px] ml-1" : 'hidden';
 
-    let classesLabelVariant = variant === 'item' ? `pb-2 md:mb-2 ${classesLabel}` : variant === 'full' ? `pb-2 ${classesLabel}` : variant === 'item-name' ? `pb-4 md:pb-2 md:mb-2 ${classesLabel}` : 'hidden'; 
+    let classesLabelVariant = variant === 'item' ? `pb-2 md:mb-2 ${classesLabel}` : variant === 'full' || variant === 'address' ? `pb-2 ${classesLabel}` : variant === 'item-name' ? `pb-4 md:pb-2 md:mb-2 ${classesLabel}` : 'hidden'; 
 
     let classesInputVariant = variant !== 'no-label' ? classesInput : ` md:mt-1 ${classesInput}`;
 
-    let classesContainer = variant === 'full' ? `pt-4 pb-2.5 ${classes}` : `pt-4 pb-2.5 md:pt-2 ${classes}`;
+    let classesContainer = variant === 'full' || variant === 'address' ? `pt-4 pb-2.5 ${classes}` : `pt-4 pb-2.5 md:pt-2 ${classes}`;
 
-    let classesWrapper = variant === 'full' ? 'flex' : variant === 'item' | variant === 'item-name' ? 'flex' :'hidden';
+    let classesWrapper = variant === 'full' || variant === 'address' ? 'flex' : variant === 'item' | variant === 'item-name' ? 'flex' :'hidden';
 
     return (
         <div className={ classesContainer }>
