355770ea1e862b6c4ae5d4bb3c211607c4f2a552
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2023-09-06 12:09:03 +0200

    hoovers for new invoice footer buttons

--- src/components/forms/FooterEditInvoice.js
+++ src/components/forms/FooterEditInvoice.js
@@ -13,8 +13,14 @@ export const FooterEditInvoice = () => {
 
     return (
         <footer className="flex items-center justify-center gap-4 rounded-xl w-full pr-12 bg-white dark:bg-dark-300 fixed md:w-[80%] md:justify-between md:px-[56px] md:ml-[-54px] bottom-0 pb-4 lg:static lg:ml-0 lg:pl-0 lg:w-full lg:pr-0 lg:mt-0 lg:mb-3">
-                <Button styles="bg-dark-500  text-neutral-300 dark:bg-dark-200 dark:text-neutral-200" type="button" title="Cancel" onClick={handleDiscard} />
-                <Button styles="bg-primary-200 text-white" type="submit" title="Save Changes" onClick={() => setFieldValue('status', 'pending')} />
+                <Button styles="bg-neutral-100 text-neutral-500 dark:bg-dark-200 dark:text-neutral-200"
+                        type="button"
+                        title="Cancel"
+                        onClick={handleDiscard} />
+                <Button styles="bg-primary-200 text-white"
+                        type="submit"
+                        title="Save Changes"
+                        onClick={() => setFieldValue('status', 'pending')} />
         </footer>
     )
 }
--- src/components/forms/FooterNewInvoice.js
+++ src/components/forms/FooterNewInvoice.js
@@ -22,25 +22,25 @@ export const FooterNewInvoice = () => {
     }
 
     return (
-        <footer className="flex items-center justify-center w-full md:justify-between gap-4 py-8 bg-white
-        dark:bg-dark-300 fixed md:px-[56px] bottom-0 left-0 pb-4 lg:static lg:ml-0 lg:pl-0 lg:pr-0 lg:mt-0 lg:mb-3">
-            <Button styles="bg-neutral-100 text-neutral-500"
+        <footer className="flex w-full items-center justify-center md:justify-between gap-4  bg-white
+        dark:bg-dark-300 fixed bottom-0 left-0 lg:static py-8 pb-4 md:px-[56px]">
+            <Button styles="bg-neutral-100 text-neutral-500 hover:bg-neutral-200"
                     type="button"
                     title="Discard"
                     onClick={handleDiscard} />
-            <Button styles="bg-dark-500  text-neutral-300 dark:bg-dark-200 dark:text-neutral-200 md:hidden"
+            <Button styles="bg-dark-500 text-neutral-300 dark:bg-dark-200 dark:text-neutral-200 hover:bg-dark-600 md:hidden"
                     type="button"
                     title="Save as Draft"
                     onClick={handleSavingAsDraft} />
-            <Button styles="bg-primary-200 text-white md:hidden"
+            <Button styles="bg-primary-200 text-white hover:bg-primary-100 md:hidden"
                     type="submit"
                     title="Save & Send"
                     onClick={() => setFieldValue('status', 'pending')} />
             <div className="hidden md:flex md:space-x-2 lg:space-x-[6px]">
-                <Button styles="bg-dark-500  text-neutral-300 dark:bg-dark-200 dark:text-neutral-200"
+                <Button styles="bg-dark-500  text-neutral-300 dark:bg-dark-200 dark:text-neutral-200 hover:bg-dark-600"
                         type="button" title="Save as Draft"
                         onClick={handleSavingAsDraft} />
-                <Button styles="bg-primary-200 text-white"
+                <Button styles="bg-primary-200 text-white hover:bg-primary-100"
                         type="submit"
                         title="Save & Send"
                         onClick={() => setFieldValue('status', 'pending')} />
