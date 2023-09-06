d8fc3a81a6b27508783f19ae6a61b5e167d9325b
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2023-09-06 11:24:13 +0200

    fixed problems with footer in new form modal

--- src/components/forms/FooterNewInvoice.js
+++ src/components/forms/FooterNewInvoice.js
@@ -22,13 +22,28 @@ export const FooterNewInvoice = () => {
     }
 
     return (
-        <footer className="flex items-center justify-center gap-4 rounded-xl w-full pr-12 bg-white dark:bg-dark-300 fixed md:w-[80%] md:justify-between md:px-[56px] md:ml-[-54px] bottom-0 pb-4 lg:static lg:ml-0 lg:pl-0 lg:w-full lg:pr-0 lg:mt-0 lg:mb-3">
-            <Button styles="bg-neutral-100 text-neutral-500" type="button" title="Discard" onClick={handleDiscard} />
-            <Button styles="bg-dark-500  text-neutral-300 dark:bg-dark-200 dark:text-neutral-200 md:hidden" type="button" title="Save as Draft" onClick={handleSavingAsDraft} />
-            <Button styles="bg-primary-200 text-white md:hidden" type="submit" title="Save & Send" onClick={() => setFieldValue('status', 'pending')} />
+        <footer className="flex items-center justify-center md:justify-between gap-4 py-8 bg-white
+        dark:bg-dark-300 fixed md:px-[56px] md:py-6 bottom-0 pb-4 lg:static lg:ml-0 lg:pl-0 lg:pr-0 lg:mt-0 lg:mb-3">
+            <Button styles="bg-neutral-100 text-neutral-500"
+                    type="button"
+                    title="Discard"
+                    onClick={handleDiscard} />
+            <Button styles="bg-dark-500  text-neutral-300 dark:bg-dark-200 dark:text-neutral-200 md:hidden"
+                    type="button"
+                    title="Save as Draft"
+                    onClick={handleSavingAsDraft} />
+            <Button styles="bg-primary-200 text-white md:hidden"
+                    type="submit"
+                    title="Save & Send"
+                    onClick={() => setFieldValue('status', 'pending')} />
             <div className="hidden md:flex md:space-x-2 lg:space-x-[6px]">
-                <Button styles="bg-dark-500  text-neutral-300 dark:bg-dark-200 dark:text-neutral-200" type="button" title="Save as Draft" onClick={handleSavingAsDraft} />
-                <Button styles="bg-primary-200 text-white" type="submit" title="Save & Send" onClick={() => setFieldValue('status', 'pending')} />
+                <Button styles="bg-dark-500  text-neutral-300 dark:bg-dark-200 dark:text-neutral-200"
+                        type="button" title="Save as Draft"
+                        onClick={handleSavingAsDraft} />
+                <Button styles="bg-primary-200 text-white"
+                        type="submit"
+                        title="Save & Send"
+                        onClick={() => setFieldValue('status', 'pending')} />
             </div>
         </footer>
     )
