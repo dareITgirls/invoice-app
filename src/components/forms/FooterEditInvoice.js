b752e9780193de936c532c2fc76e1531907542c2
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2023-09-06 14:37:13 +0200

    changed hardcoded values into tailwind predefined ones

--- src/components/forms/FooterEditInvoice.js
+++ src/components/forms/FooterEditInvoice.js
@@ -12,9 +12,7 @@ export const FooterEditInvoice = () => {
     }
 
     return (
-        // <footer className="flex items-center justify-center gap-4 rounded-xl w-full pr-12 bg-white dark:bg-dark-300 fixed md:w-[80%] md:justify-between md:px-[56px] md:ml-[-54px] bottom-0 pb-4 lg:static lg:ml-0 lg:pl-0 lg:w-full lg:pr-0 lg:mt-0 lg:mb-3">
-          <footer className="flex w-full items-center justify-center md:justify-around gap-4 bg-white
-        dark:bg-dark-300 fixed bottom-0 left-0 lg:static py-8 pb-4 md:px-[56px]">
+        <footer className="flex w-full items-center justify-center md:justify-around gap-4 bg-white dark:bg-dark-300 fixed bottom-0 left-0 lg:static py-8 pb-4 md:px-14">
                 <Button styles="bg-neutral-100 text-neutral-500 dark:bg-dark-200 dark:text-neutral-200 hover:bg-neutral-200"
                         type="button"
                         title="Cancel"
--- src/components/forms/FooterNewInvoice.js
+++ src/components/forms/FooterNewInvoice.js
@@ -22,8 +22,7 @@ export const FooterNewInvoice = () => {
     }
 
     return (
-        <footer className="flex w-full items-center justify-center md:justify-between gap-4  bg-white
-        dark:bg-dark-300 fixed bottom-0 left-0 lg:static py-8 pb-4 md:px-[56px]">
+        <footer className="flex w-full items-center justify-center md:justify-between gap-4 bg-white dark:bg-dark-300 fixed bottom-0 left-0 lg:static py-8 pb-4 md:px-14">
             <Button styles="bg-neutral-100 text-neutral-500 hover:bg-neutral-200"
                     type="button"
                     title="Discard"
