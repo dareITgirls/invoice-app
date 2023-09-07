57b9c1a5146b6e81d36a82dc416ddbed7927b4b3
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2023-09-07 10:15:01 +0200

    index.jx refactored

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+
 import { App } from './App';
 import { store } from './store/store';
 
